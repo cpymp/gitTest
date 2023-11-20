13b9c31280513e117ee60f9d1ca0a3952dc63de0
cpymp <cpy971512235@gmail.com>
2023-11-20 17:03:50 +0800

    第四次加载

--- src/domain/Users.java
+++ src/domain/Users.java
@@ -22,5 +22,11 @@ public class Users {
         for (int i = 0; i < 100 ; i++){
             System.out.println("i = : " + i);
         }
+
+        for (int i = 1 ; i <= 9;i++){
+            for (int j = 1 ; j <= i;j++){
+                System.out.println(i+"*"+j+"=" + (i*j));
+            }
+        }
     }
 }
